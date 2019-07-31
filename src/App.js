846cd350a1da838c19e0c3a0fd9a562ac6f63541
Richard Oakey <richie@Richards-MacBook-Pro.local>
2019-07-31 16:51:03 +0100

    add search component and submit prop

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Fragment, Component } from 'react'
 import './App.css'
 import Navbar from './components/layout/Navbar'
 import Users from './components/users/Users'
+import Search from './components/users/Search'
 import axios from 'axios'
 
 class App extends Component {
@@ -26,11 +27,28 @@ class App extends Component {
     })
   }
 
+  searchUsers = async text => {
+    this.setState({
+      loading: true
+    })
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    )
+
+    this.setState({
+      users: res.data.items,
+      loading: false
+    })
+  }
+
   render() {
     return (
       <Fragment>
         <Navbar />
         <div className='container'>
+          <Search searchUsers={this.searchUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </Fragment>
