49a1b1da907003ab4ce0bb01e14ea4caf529c82e
Richie Redizm <richieredizm@gmail.com>
2019-08-22 15:26:52 +0100

    create home and not found components and include with router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/layout/Navbar'
-import Users from './components/users/Users'
+import Home from './components/pages/Home'
 import UserDetail from './components/users/UserDetail'
-import Search from './components/users/Search'
 import Alert from './components/layout/Alert'
 import About from './components/pages/About'
+import NotFound from './components/pages/NotFound'
 
 import GithubState from './context/github/GithubState'
 import AlertState from './context/alert/AlertState'
@@ -35,22 +35,10 @@ const App = () => {
           <div className='container'>
             <Alert />
             <Switch>
-              <Route
-                exact
-                path='/'
-                render={props => (
-                  <Fragment>
-                    <Search />
-                    <Users />
-                  </Fragment>
-                )}
-              />
-              <Route
-                exact
-                path='/user/:login'
-                render={props => <UserDetail {...props} />}
-              />
-              <Route exact path='/about' render={About} />
+              <Route exact path='/' component={Home} />
+              <Route exact path='/about' component={About} />
+              <Route exact path='/user/:login' component={UserDetail} />
+              <Route component={NotFound} />
             </Switch>
           </div>
         </Router>
